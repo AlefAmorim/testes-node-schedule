6ea6c823b246620850b8e595f0cac6d67ac7158a
Alef <alefsantos8730@gmail.com>
2026-09-07 16:12:41 -0300

    fix: alterando o caminho para o service worker

--- Front/src/utils/register/register.js
+++ Front/src/utils/register/register.js
@@ -2,7 +2,8 @@ export async function registerServiceWorker(){
   try {
     // Fazendo o registro do serviceWorker
     // Por ser uma promise é permitido o uso de async/await
-    const registration = await navigator.serviceWorker.register('../../../public/js/service-worker.js');
+    ///sw.js porque esta na pasta public
+    const registration = await navigator.serviceWorker.register('/service-worker.js');
 
     // A chamada de register retorna um ServiceWorkerRegistration
     // que será utilizado para acesso da Push API
